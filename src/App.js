b7784405b536625337049105831e37d94a4d7d8f
pillemer <pillemer@gmail.com>
2024-01-11 16:13:00 +0000

    dislpay images in folder dynamically

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <Navigation />
-      <GalleryWall />
+      {/* <GalleryWall /> */}
       <ArtDisplay />
     </>
   )
--- src/components/ArtDisplay.jsx
+++ src/components/ArtDisplay.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Col, Image, Row } from 'antd'
+
 
 const ArtDisplay = () => {
+  const [imageList, setImageList] = useState([]);
+
+  useEffect(() => {
+    // Fetch the list of image filenames from the server
+    fetch('/image-list')
+      .then(response => response.json())
+      .then(data => setImageList(data))
+      .catch(error => console.error('Error fetching image list:', error));
+  }, []);
+
   return (
-    <div>
-        <img src='images/20210130_105905-1.jpg'/>
-    </div>
+    <Image.PreviewGroup
+      preview={{
+        onChange: (current, prev) => console.log(`current index: ${current}, prev index: ${prev}`),
+      }}
+    >
+      {/* remove expanded menu on image */}
+      <Row gutter={[16, 16]}>
+        {imageList.map((imageName, index) => (
+          <Col>
+            <Image key={`image_${index}`} width={200} src={`/images/${imageName}`} alt={`Image ${index + 1}`} />
+          </Col>
+        ))}
+      </Row>
+
+    </Image.PreviewGroup>
   )
 }
 
